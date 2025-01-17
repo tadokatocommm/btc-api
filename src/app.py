541e2be87a3783d42f0c050e17865b1aed9fd0c8
tadokatocommm <tadokatocom@gmail.com>
2025-01-17 20:16:48 -0300

    att cod

--- src/app.py
+++ src/app.py
@@ -1,5 +1,27 @@
+import os
+import time
 import requests
+import logging
+import logfire
 from datetime import datetime
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from logging import basicConfig, getLogger
+
+# Importar Base e BitcoinPreco do database.py
+from database import Base, BitcoinPreco
+
+# Carrega variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Lê as variáveis separadas do arquivo .env
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
 
 def extrair_dados_bitcoin():
     """Pega os dados da API e transforma num JSON."""
@@ -9,14 +31,15 @@ def extrair_dados_bitcoin():
 
 def tratar_dados_bitcoin(dados_json):
     # Essa função recebe o JSON armazenado na variável dados_json e processa os dados para extrair as informações desejadas.
-    valor = dados_json['data']['amount']  
+    valor = float(dados_json['data']['amount'])
     criptomoeda = dados_json['data']['base']  
     moeda = dados_json['data']['currency']  
-                
+    timestamp = datetime.now()
     dados_tratados = [{  # Organiza os dados em uma lista de dicionários
             "valor": valor,
             "criptomoeda": criptomoeda,
-            "moeda": moeda
+            "moeda": moeda,
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         }]
         
     return dados_tratados  # Retorna os dados tratados
